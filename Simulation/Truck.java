962009d9e02b611c64aa5ee6380a685392d3f22a
anthonyestephan <estephana02@moravian.edu>
2019-03-20 16:00:56 -0400

    Route works, old failed route code removed

--- Simulation/Truck.java
+++ Simulation/Truck.java
@@ -20,7 +20,8 @@ public class Truck {
         int x = xLocation;
         int y = yLocation;
         Direction d = direction;
-        int tickCount = 0;
+        int tickCount = 0; //Number of moves
+        int partial = 0; // Stores number of moves left to reach a corner
 
         Iterator<Address> iterator = addresses.iterator();
         while (iterator.hasNext()) {
@@ -29,114 +30,169 @@ public class Truck {
             int dX = address.getHouseNum() / 10;
 
             while (x != dX || y != dY) {
-                System.out.println(tickCount + " " + d);
-                if(d == Direction.North || d == Direction.South || d == Direction.Null) {
-                    if (d == Direction.Null) {//Fresh truck
-                        if (y > dY)
-                            d = Direction.North;
-                        else
-                            d = Direction.South;
-                    }
 
-                    if (d == Direction.North && dY % 10 == 0 && canMove(x, y,d)) { //House on x-axis
-                        y--;
-                        tickCount++;
-                    }
+                if(partial != 0){ //Finish moving to corner
+                    if(d == Direction.North)
+                        for(;partial != 0; partial--)
+                            y--;
+                    else if (d == Direction.South)
+                        for(;partial != 0; partial--)
+                            y++;
+                    else if (d == Direction.East)
+                        for(;partial != 0; partial--)
+                            x++;
+                    else if (d == Direction.West)
+                        for(;partial != 0; partial--)
+                            x--;
 
-                    else if (d == Direction.North && Math.abs(dY - y) >= dY % 10 && canMove(x, y,d)) { // House on y - axis
-                        y--;
-                        tickCount++;
-                    }
-
-                    else if (isCorner(x,y,d) && y >= dY){
-                        if (x > dX)
-                            d = Direction.West;
-                        else
-                            d = Direction.East;
-                    }
-
-                    else if (d == Direction.South && dY % 10 == 0 && canMove(x, y,d)) { //House on x-axis
-                        y++;
-                        tickCount++;
-                    }
-
-                    else if (d == Direction.South && Math.abs(dY - y) >= dY % 10 && canMove(x, y,d)) { // House on y - axis
-                        y++;
-                        tickCount++;
-                    }
-
-                    else if (isCorner(x, y,d) && y <= dY) {
-                        if (x > dX)
-                            d = Direction.West;
-                        else
-                            d = Direction.East;
-                    }
                 }
 
 
-                if(d == Direction.East || d == Direction.West) {
-                    if (d == Direction.West && dX % 10 == 0 && canMove(x, y,d)) { //House on x-axis
-                        x--;
-                        tickCount++;
+                if(d == Direction.Null){
+                    d = Direction.North;
+                    y--;
+                    tickCount++;
+                } else if(d == Direction.North){ // Y is correct (1), Y is above full (2), reverse (3), y is above partially (4)
+                    if(y == dY){ //On this Y level *****1*****
+                        if(x < dX) //To the right
+                            d = Direction.East;
+                        else if( x > dX) //To the left
+                            d = Direction.West;
+                    } else if( y - dY >= 10){ // Above this Y level by a full block or more *****2*****
+                        for(int i = 0; i < 10; i++){ // Move a full block up (10 ticks)
+                            tickCount++;
+                            y--;
+                        }
+                    } else if( y < dY){ // Need to be facing south *****3*****
+                        if(x < dX) //To the right
+                            d = Direction.East;
+                        else if( x > dX) //To the left
+                            d = Direction.West;
+                        else { //It is on the correct X level already
+                            if (neighborhood.getGridMarker(x + 1,y) != "  ") // If the East block is not out of bounds
+                                d = Direction.East;
+                            else //If the east block is out of bounds, the right block is not
+                                d = Direction.West;
+                        }
+                    } else if (y > dY){ // above this Y level by less than a block *****4*****
+                        if(x < dX) //To the right
+                            d = Direction.East;
+                        else if( x > dX) //To the left
+                            d = Direction.West;
+                        else { // Y move partially up, logs into partial
+                            for(int i = 0; y != dY; i++) {
+                                y--;
+                                partial = 10 - i;
+                            }
+                        }
                     }
-
-                    else if (d == Direction.West && Math.abs(dX - x) >= dX % 10 && canMove(x, y,d)) { // House on y - axis
-                        x--;
-                        tickCount++;
+                } else if(d == Direction.South){ // Y is correct (1), Y is below full (2), reverse (3), y is above partially (4)
+                    if(y == dY) { //On this Y level *****1*****
+                        if (x < dX) //To the right
+                            d = Direction.East;
+                        else if (x > dX) //To the left
+                            d = Direction.West;
+                    } else if (y - dY <= -10) { // Below this Y level by a full block or more *****2*****
+                        for(int i = 0; i < 10; i++){ // Move a full block up (10 ticks)
+                            tickCount++;
+                            y++;
+                        }
+                    } else if (y > dY) { // Need to be facing north *****3*****
+                        if(x < dX) //To the right
+                            d = Direction.East;
+                        else if( x > dX) //To the left
+                            d = Direction.West;
+                        else { //It is on the correct X level already
+                            if (neighborhood.getGridMarker(x + 1,y) != "  ") // If the East block is not out of bounds
+                                d = Direction.East;
+                            else //If the east block is out of bounds, the right block is not
+                                d = Direction.West;
+                        }
+                    } else { // below this Y level by less than a block *****4*****
+                        if(x < dX) //To the right
+                            d = Direction.East;
+                        else if( x > dX) //To the left
+                            d = Direction.West;
+                        else { // Y move partially up, logs into partial
+                            for(int i = 0; y != dY; i++) {
+                                y++;
+                                partial = 10 - i;
+                            }
+                        }
                     }
-
-                    else if(isCorner(x, y,d) && x >= dX){
-                        if (y > dY)
-                            d = Direction.North;
-                        else
+                } else if(d == Direction.East){// X is correct (1), X is below full (2), reverse (3), X is below partially (4)
+                    if(x == dX) { //On this X level *****1*****
+                        if (y < dY) //To the right
                             d = Direction.South;
-                    }
-
-                    else if (d == Direction.East && dX % 10 == 0 && canMove(x, y,d)) { //House on x-axis
-                        x++;
-                        tickCount++;
-                    }
-
-                    else if (d == Direction.East && Math.abs(dX - x) >= dX % 10 && canMove(x, y,d)) { // House on y - axis
-                        x++;
-                        tickCount++;
-                    }
-
-                    else if (isCorner(x, y,d) && x <= dX) {
-                        if (y > dY)
+                        else if (y > dY) //To the left
                             d = Direction.North;
-                        else
+                    } else if (x - dX <= -10) { // Below this X level by a full block or more *****2*****
+                        for(int i = 0; i < 10; i++){ // Move a full block up (10 ticks)
+                            tickCount++;
+                            x++;
+                        }
+                    } else if (x > dX) { // Need to be facing West *****3*****
+                        if(y < dY) //To the right
                             d = Direction.South;
+                        else if( y > dY) //To the left
+                            d = Direction.North;
+                        else { //It is on the correct X level already
+                            if (neighborhood.getGridMarker(x ,y + 1) != "  ") // If the East block is not out of bounds
+                                d = Direction.South;
+                            else //If the east block is out of bounds, the right block is not
+                                d = Direction.North;
+                        }
+                    } else { // below this X level by less than a block *****4*****
+                        if (y < dY) //To the right
+                            d = Direction.South;
+                        else if (y > dY) //To the left
+                            d = Direction.North;
+                        else { // Y move partially up, logs into partial
+                            for(int i = 0; x != dX; i++) {
+                                x++;
+                                partial = 10 - i;
+                            }
+                        }
+                    }
+                } else if(d == Direction.West){// X is correct (1), X is above full (2), reverse (3), X is above partially (4)
+                    if(x == dX) { //On this X level *****1*****
+                        if (y < dY) //To the right
+                            d = Direction.South;
+                        else if (y > dY) //To the left
+                            d = Direction.North;
+                    } else if (x - dX >= 10) { // Above this X level by a full block or more *****2*****
+                        for(int i = 0; i < 10; i++){ // Move a full block up (10 ticks)
+                            tickCount++;
+                            x--;
+                        }
+                    } else if (x < dX) { // Need to be facing West *****3*****
+                        if(y < dY) //To the right
+                            d = Direction.South;
+                        else if( y > dY) //To the left
+                            d = Direction.North;
+                        else { //It is on the correct X level already
+                            if (neighborhood.getGridMarker(x ,y + 1) != "  ") // If the East block is not out of bounds
+                                d = Direction.South;
+                            else //If the east block is out of bounds, the right block is not
+                                d = Direction.North;
+                        }
+                    } else { // above this X level by less than a block *****4*****
+                        if (y < dY) //To the right
+                            d = Direction.South;
+                        else if (y > dY) //To the left
+                            d = Direction.North;
+                        else { // Y move partially up, logs into partial
+                            for(int i = 0; x != dX; i++) {
+                                x--;
+                                partial = 10 - i;
+                            }
+                        }
                     }
                 }
             }
         }
 
         return tickCount;
-    }
-
-    private Boolean isCorner(int x, int y, Direction d){
-        if(d == Direction.North || d == Direction.South){
-            if(( x + 1 < 201 && neighborhood.getGridMarker(x + 1,y) != "  " ) || (x - 1 > -1 && neighborhood.getGridMarker(x - 1,y) != "  "))
-                return true;
-        }
-        if(d == Direction.East || d == Direction.West){
-            if ((y + 1 < 201 && neighborhood.getGridMarker(x,y + 1) != "  " ) || (y - 1 > -1 && neighborhood.getGridMarker(x ,y - 1) != "  "))
-               return true;
-        }
-        return false;
-    }
-
-    private Boolean canMove(int x, int y, Direction d){
-        if(d == Direction.North && y - 1 < -1 && neighborhood.getGridMarker(x ,y - 1) != "  ")
-            return true;
-        if(d == Direction.South && y + 1 < 201 && neighborhood.getGridMarker(x ,y + 1) != "  ")
-            return true;
-        if(d == Direction.West && x + 1 < 201 && neighborhood.getGridMarker(x + 1 ,y) != "  ")
-            return true;
-        if(d == Direction.East && x - 1 > -1 && neighborhood.getGridMarker(x - 1,y) != "  ")
-            return true;
-        return false;
     }
 
     public int getXLocation() {
