6fc80b713c30d5aa764a3382298bba80cdf38edd
anthonyestephan <estephana02@moravian.edu>
2019-04-26 12:15:56 -0400

    mismatched input error catch added for user input

--- Simulation/Drivers/Main.java
+++ Simulation/Drivers/Main.java
@@ -11,6 +11,7 @@ import Simulation.Nouns.Neighborhood;
 import Simulation.Nouns.SimulationRunner;
 import Simulation.Nouns.Truck;
 
+import java.util.InputMismatchException;
 import java.util.PriorityQueue;
 
 public class Main {
@@ -18,7 +19,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         // Write 100 random addresses to a file
-        //AddressIO.writeAddresses(AddressIO.FILE, 100);
+        //AddressIO.writeAddresses(AddressIO.FILE, 102); //second input is how many addresses to randomly create2
 
         // Read the addresses from the file and place them in a PriorityQueue
         PriorityQueue<Address> addresses = AddressIO.readAddresses(AddressIO.FILE);
@@ -26,19 +27,19 @@ public class Main {
         // Draw the neighborhood with the addresses and distribution center shown
         Neighborhood neighborhood = new Neighborhood();
         Truck truck = new Truck(neighborhood);
+
+        try {
+            truck.userInput();
+        } catch (InputMismatchException e) {
+            System.out.println("Letter or word was entered, Please enter a 1 or 2.");
+            truck.userInput();
+        }
+
         System.out.println(truck.route(addresses) + " " + truck.seeRoute());
         System.out.println(truck.routeTime(addresses) + " " + truck.seeRouteTime());
-
-        truck.userInput();
 
         SimulationRunner sr = new SimulationRunner();
         truck.registerObservers(sr);
-        /*
-        truck.setRoute(new RouteRightDistance());
-        truck.setRouteTime(new RouteRightTime());
-        System.out.println(truck.route(addresses) + " " + truck.seeRoute());
-        System.out.println(truck.routeTime(addresses) + " " + truck.seeRouteTime());
-        */
 
         //truck.route resets move queue (in case anything was in it) and adds moves to queue
         while(truck.canMove()) { //MUST RUN A truck.route BEFORE RUNNING GUI
